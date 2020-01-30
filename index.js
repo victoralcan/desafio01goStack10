3668153f51c19cd2c62e017a20e59c491a19f282
Victor <victoralcan@gmail.com>
2020-01-30 15:51:47 -0300

    Implementando middlewares. Desafio Finalizado

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const express = require('express');
 const server = express();
 
 const projetos = [];
+ 
+let requisicoes = 0;
 
 server.listen(3000);
 
 server.use(express.json());
+server.use((req,res,next) => {
+  requisicoes++;
+  console.log(requisicoes);
+  next();
+});
 
-server.post('/projects',addProjeto);
-server.get('/projects',listaTodosProjetos);
-server.put('/projects/:id',alteraTitulo);
-server.delete('/projects/:id',deletaProjeto);
+server.post('/projects', existeIdBody, addProjeto);
+server.get('/projects', listaTodosProjetos);
+server.put('/projects/:id', existeIdParams, alteraTitulo);
+server.delete('/projects/:id', existeIdParams, deletaProjeto);
+server.post('/projects/:id/tasks', addTarefa);
 
 
 function addProjeto(req,res){
@@ -28,7 +36,10 @@ function listaTodosProjetos(req,res){
 function alteraTitulo(req, res){
   const { id } = req.params;
 
-  const index = projetos.findIndex((e) => e.id = id);
+  const index = projetos.findIndex((e) => e.id === id);
+  if(index === -1){
+    return res.status(400).json("Projeto não encontrado");
+  }
   projetos[index].title = id;
 
   return res.json(projetos);
@@ -37,8 +48,40 @@ function alteraTitulo(req, res){
 function deletaProjeto(req,res){
   const { id } = req.params;
 
-  const index = projetos.findIndex((e) => e.id = id);
-  projetos.splice(index,1);
+  const index = projetos.findIndex((e) => e.id === id);
 
+  if(index === -1){
+    return res.status(400).json("Projeto não encontrado");
+  }
+
+  projetos.splice(index,1);
   return res.json(projetos);
 }
+
+function existeIdParams(req,res,next){
+  if(!req.params.id){
+    return res.status(400).json("Id is required!");
+  }
+  return next();
+}
+
+function existeIdBody(req,res,next){
+  if(!req.body.id){
+    return res.status(400).json("Id is required!");
+  }
+  return next();
+}
+
+function addTarefa(req,res){
+  const { id } = req.params;
+  const { title } = req.body; 
+
+  const index = projetos.findIndex((e) => e.id === id);
+  if(index === -1){
+    return res.status(400).json("Projeto não encontrado");
+  }
+
+  projetos[index].tasks.push(title);
+  return res.json(projetos[index]);
+
+ }
